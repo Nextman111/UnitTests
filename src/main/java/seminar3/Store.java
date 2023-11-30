a4e91eaa8cce44aafd4e51c11d20d9d9b644029b
VladislavRazvodov <g-man-111@ya.ru>
2023-11-30 23:54:05 +0300

    add seminar3 prepare

---
+++ src/main/java/seminar3/Store.java
@@ -0,0 +1,33 @@
+package seminar3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Store {
+    private List<Product> inventory;
+
+    public Store() {
+        inventory = new ArrayList<>();
+    }
+
+    public void addProductToInventory(Product product) {
+        inventory.add(product);
+    }
+
+    public void removeProductFromInventory(Product product) {
+        inventory.remove(product);
+    }
+
+    public Product findProductByName(String name) {
+        for (Product product : inventory) {
+            if (product.getName().equalsIgnoreCase(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public Cart createCart() {
+        return new Cart();
+    }
+}
